cfa11dea3530bcdaf6a02fd98d28331901ee2db5
Dean P. Jones <61812@ACDM.DS.SAIT.CA>
2016-10-31 22:06:33 -0600

    footer.php, add for ease for all pages

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
 <?php
 	#FOOTER
-	print("<h4>Travel Experts Inc. - Copyright 2016 &copy;</h4>");
+	print("<footer>");
+		print("<h4>Travel Experts Inc. - Copyright 2016 &copy;</h4>");
+	print("</footer>");
+	/* <h4>Home - Travel Experts Inc. - Copyright 2016 &copy;</h4> */
 ?>
