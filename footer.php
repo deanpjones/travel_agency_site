8154cc5667c3cbd602b8ac527d5a57e36ecaa1b2
djones <pythagoras.drafting@gmail.com>
2016-11-01 14:06:54 -0600

    footer.php, added $page_desc

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <?php
 	#FOOTER
+	#$page_desc = "";	#INCLUDE ON PAGE otherwise this will overwrite
+	
 	print("<footer>");
-		print("<h4>Travel Experts Inc. - Copyright 2016 &copy;</h4>");
+		#print("<h4>Travel Experts Inc. - Copyright 2016 &copy;</h4>");
+		print("<h4>" . $page_desc . " - Travel Experts Inc. - Copyright 2016 &copy;</h4>");
 	print("</footer>");
 	/* <h4>Home - Travel Experts Inc. - Copyright 2016 &copy;</h4> */
 ?>
