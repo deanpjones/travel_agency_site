7fd7059b5e3d760689b268c1f80471443dacbbfa
Dean P. Jones <61812@ACDM.DS.SAIT.CA>
2016-10-31 22:08:19 -0600

    navbar.php, added for ease of all pages

---
+++ navbar.php
@@ -0,0 +1,7 @@
+	<!-- <nav id="image_background"> -->
+	<nav>
+		<a href="index.php" class="navbar">Home</a>
+		<a href="register.php" class="navbar">Register</a>
+		<a href="contact.php" class="navbar">Contact</a>
+		<a href="links.php" class="navbar">Links</a>
+	</nav>
