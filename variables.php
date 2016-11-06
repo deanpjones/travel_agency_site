c215fa029debfe0fd0c8feb7ae877a3bcc4d25a2
Dean P. Jones <61812@ACDM.DS.SAIT.CA>
2016-11-06 16:56:18 -0700

    variables.php, updates

--- variables.php
+++ variables.php
@@ -5,6 +5,21 @@
 	$password = "password";
 	$database = "travelexperts";
 	
+	#DB CONNECT
 	#$dbh = mysqli_connect($host, $user, $password, $database); 
+	
+	//LINKS ARRAY (links.php)
+			$array_links = array(
+				"Sky News"=>"http://www.skynews.ca/this-weeks-sky",
+				"Endless Nature"=>"http://endlessnature.org/", 
+				"West Coast Dream - Ucluelet Canada"=>"http://www.ucluelet-info.com", 
+				"Visit Norway"=>"https://www.visitnorway.com/plan-your-trip/seasons-climate/autumn/", 
+				"White Water Rafting Adventure"=>"http://chinookrafting.com/",
+				"Sightseeing on the wild West Coast!"=>"https://westcoastsightseeing.com", 
+				"Stars in the Sky!"=>"http://www.stellarium.org/en_CA/", 
+				"Ocean Tours"=>"http://www.oceanstravel.ca/", 
+				"Beautiful Hawaii"=>"http://www.gohawaii.com/", 
+				"Down under in Austrailia"=>"http://www.australia.com/en-ca"
+			);
 	
 ?>
