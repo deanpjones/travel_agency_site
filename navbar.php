88810efeecf014349efd6816ce8cca1712a92805
djones <pythagoras.drafting@gmail.com>
2016-11-09 15:50:26 -0700

    navbar.php, added tooltips

--- navbar.php
+++ navbar.php
@@ -1,8 +1,8 @@
 	<!-- <nav id="image_background"> -->
 	<nav>
-		<a href="index.php" class="navbar">Home</a>
-		<a href="customer_form_register.php" class="navbar">Register</a>
-		<a href="contact.php" class="navbar">Contact</a>
-		<a href="links.php" class="navbar">Links</a>
-		<a href="customer_form_update.php" class="navbar">CustUpdate</a>
+		<a href="index.php" class="navbar" style="cursor:pointer;" title="Home Page">Home</a>
+		<a href="customer_form_register.php" class="navbar" style="cursor:pointer;" title="New Customer Registration">Register</a>
+		<a href="contact.php" class="navbar" style="cursor:pointer;" title="Contact Us">Contact</a>
+		<a href="links.php" class="navbar" style="cursor:pointer;" title="Links">Links</a>
+		<a href="customer_form_list.php" class="navbar" style="cursor:pointer;" title="Customer Update Info">CustUpdate</a>
 	</nav>
