8db84f85ca410306066507fdd7d3ebb8bf8888f1
djones <pythagoras.drafting@gmail.com>
2016-11-09 15:51:06 -0700

    db_connect.php, added so I can use INCLUDE instead of rewriting this all the time

---
+++ db_connect.php
@@ -0,0 +1,26 @@
+<?php
+
+	//GLOBAL VARIABLES
+	include("variables.php");
+	
+#-------------------------------------------------------------
+#DB CONNECT
+	global $host, $user, $password, $database;
+	$dbh = mysqli_connect($host, $user, $password, $database);
+		#ERROR IF DATABASE DOESN'T CONNECT
+		if(!$dbh)											#IF (NOT CONNECTED) TO DATABASE
+		{
+			#CREATE A (LOG FILE)
+			$file = fopen("log/errorlog.txt", "a");			#OPENS FILE (OR CREATES)(TO APPEND)
+			fwrite($file, mysqli_connect_error() . "\n");	#WRITES TO FILE
+			fclose($file);									#CLOSES FILE
+			exit();											#MAKES SURE IT DOESN'T CONTINUE WITHOUT A CONNECTION
+		}
+		else
+		{
+			#print("*** SQL DATABASE CONNECTED ***<br />");
+			error_log("*** SQL DATABASE CONNECTED ***");
+		}			
+#END DB CONNECT
+#-------------------------------------------------------------
+?>
