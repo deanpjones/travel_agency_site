64c3807a42d080244b7e0200d35dd3c01cef9e6d
djones <pythagoras.drafting@gmail.com>
2016-11-11 17:16:30 -0700

    navbar_agent.php

---
+++ navbar_agent.php
@@ -0,0 +1,5 @@
+	<!-- <nav id="image_background"> -->
+	<nav>
+		<a href="customer_form_list.php" class="navbar" style="cursor:pointer;" title="Customer Update Info">Customer Update</a>
+		<a href="agent_form_list.php" class="navbar" style="cursor:pointer;" title="Agent Update Info">Agent Update</a>
+	</nav>
